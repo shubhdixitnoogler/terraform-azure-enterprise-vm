565805f80e902742878937f7a27d2ef12b84d59a
shubhdixitnoogler <shubhdixittab@gmail.com>
2026-06-29 14:39:38 +0530

    feat: update network security group rules

--- modules/network_security_group/main.tf
+++ modules/network_security_group/main.tf
@@ -5,13 +5,46 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = var.resource_group_name
 
   tags = var.tags
+}
 
+resource "azurerm_network_security_rule" "allow_http" {
+
+  name                        = "Allow-HTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "80"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
+}
+
+resource "azurerm_network_security_rule" "allow_https" {
+
+  name                        = "Allow-HTTPS"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "443"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
 
-  subnet_id = var.subnet_id
-
+  subnet_id                 = var.subnet_id
   network_security_group_id = azurerm_network_security_group.this.id
-
 }
