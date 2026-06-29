cc1b6520019cea1fe1dbc0370e2530c9ca4fd93c
shubhdixitnoogler <shubhdixittab@gmail.com>
2026-06-29 16:06:46 +0530

    location update

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 module "resource_group" {
 
-  source = "./modules/resource_group"
+  source              = "./modules/resource_group"
   resource_group_name = var.resource_group_name
-  location = var.location
-  tags = var.tags
+  location            = var.location
+  tags                = var.tags
 }
 module "virtual_network" {
 
-  source = "./modules/virtual_network"
+  source               = "./modules/virtual_network"
   virtual_network_name = var.virtual_network_name
-  resource_group_name = module.resource_group.resource_group_name
-  location = module.resource_group.location
-  address_space = var.address_space
-  tags = var.tags
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.location
+  address_space        = var.address_space
+  tags                 = var.tags
 }
 
 module "subnet" {
   source = "./modules/subnet"
 
-  resource_group_name  = module.resource_group.resource_group_name
-  virtual_network_name = module.virtual_network.virtual_network_name
+  resource_group_name     = module.resource_group.resource_group_name
+  virtual_network_name    = module.virtual_network.virtual_network_name
   vm_subnet_name          = var.vm_subnet_name
   vm_subnet_prefixes      = var.vm_subnet_prefixes
   bastion_subnet_prefixes = var.bastion_subnet_prefixes
@@ -27,20 +27,23 @@ module "subnet" {
 
 module "network_security_group" {
 
-  source = "./modules/network_security_group"
+  source                      = "./modules/network_security_group"
   network_security_group_name = var.network_security_group_name
-  resource_group_name = module.resource_group.resource_group_name
-  location = module.resource_group.location
-  subnet_id = module.subnet.vm_subnet_id
-  tags = var.tags
+  resource_group_name         = module.resource_group.resource_group_name
+  location                    = module.resource_group.location
+  subnet_id                   = module.subnet.vm_subnet_id
+  tags                        = var.tags
 
 }
 
 module "storage_account" {
 
-  source = "./modules/storage_account"
+  source               = "./modules/storage_account"
   storage_account_name = var.storage_account_name
-  resource_group_name = module.resource_group.resource_group_name
-  location = module.resource_group.location
-  tags = var.tags
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.location
+  tags                 = var.tags
+  depends_on = [
+    module.resource_group
+  ]
 }
