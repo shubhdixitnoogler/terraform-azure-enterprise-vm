c0338de6695c3f070d06f827645641d4d1faec9f
Shubh <shubhdixittab@gmail.com>
2026-09-19 21:53:08 +0000

    Reset Terraform project for rebuild

--- modules/virtual_network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "virtual_network_id" {
-  description = "Virtual Network ID"
-  value       = azurerm_virtual_network.this.id
-}
-
-output "virtual_network_name" {
-  description = "Virtual Network Name"
-  value       = azurerm_virtual_network.this.name
-}
